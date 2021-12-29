e6002cbdc2522e132e95c490ebfad54da32db8d4
shirou <shirou.faw@gmail.com>
2021-12-29 14:10:41 +0000

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ['Sphinx>=1.6', 'setuptools']
 
 setup(
     name='sphinxcontrib_indesignbuilder',
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/shirou/sphinxcontrib-indesignbuilder',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-indesignbuilder',
     license='BSD',
